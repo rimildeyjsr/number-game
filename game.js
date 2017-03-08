66e02bea353d4edca0999f24f9eac2fbf4a886c1
rimildeyjsr <rimildeyjsr@gmail.com>
2017-03-08 10:39:00 +0530

    working, make it prettier

--- game.js
+++ game.js
@@ -5,14 +5,21 @@ $(document).ready(function(){
 
         if(amount < 100)
         {
-            console.log("done");
+            //console.log("done");
+            $('.randomNo').html("");
+            $('.score').html("");
+            $('.fillInfo').html('GAME OVER!!');
             return;
         }
 
         var no1, no2, no3;
+
         no1 = Math.ceil((Math.random()*10));
         no2 = Math.ceil((Math.random()*10));
         no3 = Math.ceil((Math.random()*10));
+
+        $('.randomNo').html(no1 + " "+ no2  + " " + no3 );
+        $('.score').html(amount);
 
         console.log(no1+" "+no2+" "+no3+" ");
         console.log("initial "+amount);
@@ -20,22 +27,31 @@ $(document).ready(function(){
         if((even(no1) && even(no2) && even(no3)) || (odd(no1) && odd(no2) && odd(no3)))
         {
             amount += 300;
-            console.log("change "+amount);
+            //console.log("change "+amount);
+            $('.score').html(amount);
+            $('.fillInfo').html('Lucky! You got 300 bucks more!');
+
         }
         else if(ifSequence(no1,no2,no3))
         {
             amount += 800;
-            console.log("change "+amount);
+            //console.log("change "+amount);
+            $('.score').html(amount);
+            $('.fillInfo').html('Awesome! You got 800 bucks more!');
         }
         else if((no1 == no2) && (no2 == no3))
         {
             amount += 1000;
-            console.log("change "+amount);
+            //console.log("change "+amount);
+            $('.score').html(amount);
+            $('.fillInfo').html('Super! You got 1000 bucks more!');
         }
         else
         {
             amount += 0;
-            console.log("change "+amount);
+            //console.log("change "+
+            $('.score').html(amount);
+            $('.fillInfo').html('Bad Luck!');
         }
 
     });
